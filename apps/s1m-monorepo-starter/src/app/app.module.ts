50fda41b138eb8819b8f8d9c71a0fa89b17c9282
Simon Phumin Schweikert <git@simonschweikert.com>
2022-02-26 17:08:57 +0100

    ➕ Added nest.js and ngrx

--- apps/s1m-monorepo-starter/src/app/app.module.ts
+++ apps/s1m-monorepo-starter/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/s1m-monorepo-starter'],
+    projects: [
+        '<rootDir>/apps/s1m-monorepo-starter',
+        '<rootDir>/apps/api',
+        '<rootDir>/libs/environments',
+        '<rootDir>/libs/core',
+        '<rootDir>/libs/stores/users'
+    ]
 };
