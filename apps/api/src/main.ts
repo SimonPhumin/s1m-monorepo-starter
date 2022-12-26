aa759687a25fc828510ea434be0ac2a83ec8cb6a
Simon Phumin Schweikert <git@simonschweikert.com>
2022-12-26 17:40:10 +0100

    ➕ added serverless config for AWS Lambda deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,18 +5,36 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { ServerlessNestjsApplicationFactory } from 'serverless-lambda-nestjs';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3333;
-    await app.listen(port);
-    Logger.log(
-        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-    );
+	const app = await NestFactory.create(AppModule);
+	const globalPrefix = 'api';
+	app.setGlobalPrefix(globalPrefix);
+	const port = process.env.PORT || 3333;
+	await app.listen(port);
+	Logger.log(
+		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+	);
 }
 
-bootstrap();
+// Run Nestjs application locally
+if (process.env.NX_CLI_SET) {
+	bootstrap();
+}
+
+// Run Nestjs application in AWS Lambda
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+	const app = new ServerlessNestjsApplicationFactory<AppModule>(AppModule, {
+		// NestFactory.create's option object
+		cors: {
+			origin: '*',
+			allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
+		}
+	});
+	const result = await app.run(event, context);
+	return result;
+};
