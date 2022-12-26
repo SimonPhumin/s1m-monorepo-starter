139cd35c4997628ba88cc35c6e5b3d66d01babe8
Simon Phumin Schweikert <git@simonschweikert.com>
2022-12-26 17:22:03 +0100

    🔥 removed login app, refactored environments (dev/prod)

--- libs/environments/src/index.ts
+++ libs/environments/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/environments.module';
+import { environment as devEnvironment } from './lib/environment.dev';
+import { environment as prodEnvironment } from './lib/environment.prod';
+
+export { devEnvironment, prodEnvironment };
