4496c60d864c454ab4fbbec924d587bf4540cd36
Simon Phumin Schweikert <git@simonschweikert.com>
2022-12-26 14:25:10 +0100

    ✨ WIP: added users feature

---
+++ libs/common/features/users/src/lib/common-features-users.spec.ts
@@ -0,0 +1,7 @@
+import { commonFeaturesUsers } from './common-features-users';
+
+describe('commonFeaturesUsers', () => {
+	it('should work', () => {
+		expect(commonFeaturesUsers()).toEqual('common-features-users');
+	});
+});
