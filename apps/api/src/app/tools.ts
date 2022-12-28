ac1873794d1a6f34cc841358d56da70b618825b3
Simon Phumin Schweikert <git@simonschweikert.com>
2022-12-28 19:45:22 +0100

    ♻️ refactored serverless integration

---
+++ apps/api/src/app/tools.ts
@@ -0,0 +1,15 @@
+// import * as fs from 'fs';
+// import { CommandResult } from '@s1m/common/shared/data';
+
+// export const readJsonFile = (path: string) => {
+// 	const data: Buffer = fs.readFileSync(path);
+
+// 	return JSON.parse(data.toString());
+// };
+
+// export const createResponse = (code: number, msg: string): CommandResult => ({
+// 	status: {
+// 		code,
+// 		msg
+// 	}
+// });
