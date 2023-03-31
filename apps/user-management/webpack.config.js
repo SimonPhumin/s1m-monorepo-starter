d7f5c7a4a4d61ede0bf26506484affad0efcbaaf
Simon Phumin Schweikert <git@simonschweikert.com>
2023-03-31 20:01:20 +0200

    dependencies upgraded & webpack node polyfill error fixed

--- apps/user-management/webpack.config.js
+++ apps/user-management/webpack.config.js
@@ -1,4 +1,5 @@
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const mf = require('@angular-architects/module-federation/webpack');
 const path = require('path');
 const share = mf.share;
@@ -13,77 +14,78 @@ const share = mf.share;
  * the location of the generated temporary tsconfig file.
  */
 const tsConfigPath =
-    process.env.NX_TSCONFIG_PATH ??
-    path.join(__dirname, '../../tsconfig.base.json');
+	process.env.NX_TSCONFIG_PATH ??
+	path.join(__dirname, '../../tsconfig.base.json');
 
 const workspaceRootPath = path.join(__dirname, '../../');
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
-    tsConfigPath,
-    [
-        /* mapped paths to share */
-    ],
-    workspaceRootPath
+	tsConfigPath,
+	[
+		/* mapped paths to share */
+	],
+	workspaceRootPath
 );
 
 module.exports = {
-    output: {
-        uniqueName: 'user-management',
-        publicPath: 'auto'
-    },
-    optimization: {
-        runtimeChunk: false
-    },
-    experiments: {
-        outputModule: true
-    },
-    resolve: {
-        alias: {
-            ...sharedMappings.getAliases()
-        }
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            library: {
-                type: 'module'
-            },
-            remotes: {
-                login: 'http://localhost:4201/remoteEntry.js'
-            },
-            shared: share({
-                '@angular/core': {
-                    singleton: true,
-                    strictVersion: true,
-                    requiredVersion: 'auto',
-                    includeSecondaries: true
-                },
-                '@angular/common': {
-                    singleton: true,
-                    strictVersion: true,
-                    requiredVersion: 'auto',
-                    includeSecondaries: true
-                },
-                '@angular/common/http': {
-                    singleton: true,
-                    strictVersion: true,
-                    requiredVersion: 'auto',
-                    includeSecondaries: true
-                },
-                '@angular/router': {
-                    singleton: true,
-                    strictVersion: true,
-                    requiredVersion: 'auto',
-                    includeSecondaries: true
-                },
-                rxjs: {
-                    singleton: true,
-                    strictVersion: true,
-                    requiredVersion: 'auto',
-                    includeSecondaries: true
-                },
-                ...sharedMappings.getDescriptors()
-            })
-        }),
-        sharedMappings.getPlugin()
-    ]
+	output: {
+		uniqueName: 'user-management',
+		publicPath: 'auto'
+	},
+	optimization: {
+		runtimeChunk: false
+	},
+	experiments: {
+		outputModule: true
+	},
+	resolve: {
+		alias: {
+			...sharedMappings.getAliases()
+		}
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			library: {
+				type: 'module'
+			},
+			remotes: {
+				login: 'http://localhost:4201/remoteEntry.js'
+			},
+			shared: share({
+				'@angular/core': {
+					singleton: true,
+					strictVersion: true,
+					requiredVersion: 'auto',
+					includeSecondaries: true
+				},
+				'@angular/common': {
+					singleton: true,
+					strictVersion: true,
+					requiredVersion: 'auto',
+					includeSecondaries: true
+				},
+				'@angular/common/http': {
+					singleton: true,
+					strictVersion: true,
+					requiredVersion: 'auto',
+					includeSecondaries: true
+				},
+				'@angular/router': {
+					singleton: true,
+					strictVersion: true,
+					requiredVersion: 'auto',
+					includeSecondaries: true
+				},
+				rxjs: {
+					singleton: true,
+					strictVersion: true,
+					requiredVersion: 'auto',
+					includeSecondaries: true
+				},
+				...sharedMappings.getDescriptors()
+			})
+		}),
+		new NodePolyfillPlugin(),
+		sharedMappings.getPlugin()
+	]
 };
