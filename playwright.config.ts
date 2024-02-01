84de9ba15a188f52348be51acd274b05ae6c46e1
Simon Phumin Schweikert <git@simonschweikert.com>
2024-02-01 17:42:59 +0100

    new config files included

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test';
+const baseConfig: PlaywrightTestConfig = {
+	use: {
+		screenshot: 'only-on-failure',
+		ignoreHTTPSErrors: true,
+		baseURL: 'http://host.docker.internal:4400',
+		headless: true,
+		testIdAttribute: 'data-test-id',
+	},
+	projects: [
+		{ name: 'desktop-chrome', use: { ...devices['Desktop Chrome'] } },
+		{
+			name: 'desktop-webkit',
+			use: { ...devices['Desktop Safari'], deviceScaleFactor: 1 },
+		},
+		{
+			name: 'iphone-11-chrome',
+			use: {
+				...devices['iPhone 11'],
+				browserName: 'chromium',
+				defaultBrowserType: 'chromium',
+			},
+		},
+		{
+			name: 'iphone-11-webkit',
+			use: { ...devices['iPhone 11'] },
+		},
+	],
+	outputDir: 'test-reports/visual-regression',
+	workers: 3,
+	testMatch: /.*vr-spec.ts/,
+};
+export default baseConfig;
