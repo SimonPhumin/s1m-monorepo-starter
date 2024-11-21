0bab05754e3fb0ebf57f728bc15e2d63b5dfa081
Simon Phumin Schweikert <git@simonschweikert.com>
2024-11-21 22:08:16 +0100

    component library added

--- libs/common/features/users/src/lib/common-features-users.spec.ts
+++
@@ -1,7 +0,0 @@
-import { commonFeaturesUsers } from './common-features-users';
-
-describe('commonFeaturesUsers', () => {
-	it('should work', () => {
-		expect(commonFeaturesUsers()).toEqual('common-features-users');
-	});
-});
